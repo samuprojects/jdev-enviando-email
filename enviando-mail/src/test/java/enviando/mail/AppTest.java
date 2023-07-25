c767c6d289a98ae7930604e2b762a128c62cb2e7
samuel <samugit.projects@gmail.com>
2023-07-25 17:40:05 -0300

    Aula19_04 Configurando conexão com servidor Gmail

--- enviando-mail/src/test/java/enviando/mail/AppTest.java
+++ enviando-mail/src/test/java/enviando/mail/AppTest.java
@@ -2,14 +2,21 @@ package enviando.mail;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+	
+	private String userName = "inserir gmail aqui";
+	private String senha = "inserir senha do gmail aqui";
 
 	@org.junit.Test
 	public void testeEmail() {
+		try {
 		
 		// Observar configurações do smtp do email para testar sem problemas de bloqueio
 		Properties properties= new Properties();
@@ -19,5 +26,19 @@ public class AppTest
 		properties.put("mail.smtp.port", "465"); // Porta do servidor
 		properties.put("mail.smtp.socketFactory.port", "465"); //Especificação da porta a ser conectada pelo socket
 		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Classe socket de conexão ao SMTP")
+	
+		@SuppressWarnings("unused")
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+		
+		} catch (Exception e){
+			e.printStackTrace();
+			
+		}
 	}
+	
 }
